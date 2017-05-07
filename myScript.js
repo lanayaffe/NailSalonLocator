d252f8c63566b74e4adde377594c315b392a029c
lanayaffe <y003632c@student.staffs.ac.uk>
2017-05-07 13:37:09 +0100

    INITIAL COMMIT

---
+++ myScript.js
@@ -0,0 +1,33 @@
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+const output = document.getElementById("my-location");
+
+function success(pos) {
+	
+	let latitude  = pos.coords.latitude;
+                        let longitude = pos.coords.longitude;
+	let accuracy = pos.coords.accuracy;
+
+    output.innerHTML = `Latitude is ${latitude} and Longitude is ${longitude}. More or less ${accuracy} metres.`;
+
+    const img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+
+};
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+ const img = new Image();
+    const myLoc = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=13&size=300x300&maptype=roadmap&markers=color:blue%7Clabel:A%7C ${latitude},${longitude}`;
+img.src = myLoc;
+
