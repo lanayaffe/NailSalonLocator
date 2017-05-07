d6ac709751135e5898fe9b902db40e2e70e2b291
lanayaffe <y003632c@student.staffs.ac.uk>
2017-05-07 17:14:12 +0100

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -11,3 +11,17 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
+    })
+  );
+});
