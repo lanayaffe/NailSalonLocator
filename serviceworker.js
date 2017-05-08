b2b0b1eb659b21b43153c98ba530b641082182c5
lanayaffe <y003632c@student.staffs.ac.uk>
2017-05-08 02:11:25 +0100

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,11 @@
-var CACHE_NAME = 'gih-cache-v5';
+var CACHE_NAME = 'gih-cache-v4';
 var CACHED_URLS = [
-  // Our HTML
-  'index.html',
-  // Stylesheets and fonts
-    'style.css',
-  // JavaScript
-  // Images
-    'browse.png'
+  'offline.html',
+  'style.css'
 ];
 
+
 self.addEventListener('install', function(event) {
-  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(CACHED_URLS);
@@ -19,30 +14,17 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  var requestURL = new URL(event.request.url);
-  if (requestURL.pathname === 'index.html') {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match('index.html').then(function(cachedResponse) {
-          var fetchPromise = fetch('index.html').then(function(networkResponse) {
-            cache.put('index.html', networkResponse.clone());
-            return networkResponse;
-          });
-          return cachedResponse || fetchPromise;
-        });
-      })
-    );
-  } else if (
-    CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match(event.request).then(function(response) {
-          return response || fetch(event.request);
-        })
-      })
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
+    })
+  );
 });
 
 self.addEventListener('activate', function(event) {
@@ -50,7 +32,7 @@ self.addEventListener('activate', function(event) {
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith('gih-cache')) {
             return caches.delete(cacheName);
           }
         })
@@ -58,3 +40,7 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+
+
+
